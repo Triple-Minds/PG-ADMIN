585e3b71118a4c924a92a7f7876e31d9f731e5ce
Sameerul-Hak <sameerulhakofficial@gmail.com>
2024-10-22 00:36:24 +0530

    checking auto deploy test 1

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
       <Navbar />
       <div className="main">
         {/* Main content can go here */}
-        <h1>auto deploy sucess</h1>
+        <h1>auto deploy success</h1>
       </div>
       <div className="right_calendar">
         {/* Calendar or other right content */}
